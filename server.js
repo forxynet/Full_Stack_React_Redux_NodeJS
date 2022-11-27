fc1fb665b9fd1a4f982e696d3a7280ea8e485934
Hakan Baştürk <forxynet@gmail.com>
2022-11-28 01:38:55 +0300

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 4000;
+// Define Routes
+app.use('/api/users', require('./routers/api/users'));
+app.use('/api/auth', require('./routers/api/auth'));
+app.use('/api/posts', require('./routers/api/posts'));
+app.use('/api/profile', require('./routers/api/profile'));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
